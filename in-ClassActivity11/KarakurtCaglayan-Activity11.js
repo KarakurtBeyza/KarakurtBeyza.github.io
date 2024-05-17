e116117b880776d2192e8ed3ba2e60e58e4cf3e9
Beyza Karakurt <125734176+KarakurtBeyza@users.noreply.github.com>
2024-05-17 16:00:08 +0300

    Add files via upload

---
+++ in-ClassActivity11/KarakurtCaglayan-Activity11.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  
+    $('#nav_list a').click(function(event) {
+        event.preventDefault(); 
+
+        var speakerTitle = $(this).attr('title');
+        var fileName = 'json_files/' + speakerTitle + '.json';
+
+        $.ajax({
+            url: fileName,
+            dataType: 'json',
+            success: function(data) {
+                $('main').empty();
+                $.each(data.speakers, function(index, speaker) {
+                
+                    var speakerHTML = "<h1>" + speaker.title + "</h1>" +
+                                      "<h2>" + speaker.month + "<br>" + speaker.speaker + "</h2>" +
+                                      "<p>" + speaker.text + "</p>" +
+                                      "<img src='" + speaker.image + "' alt='" + speaker.title + "'>";
+                    $('main').append(speakerHTML);
+                });
+            },
+            error: function() {
+                alert("Error loading the speaker data.");
+            }
+        });
+    });
+}); // end ready
